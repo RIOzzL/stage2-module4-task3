102518e968027b7849d2ea7b2e67eca586e735e9
Roman Gulevich <r.i.gulevich@gmail.com>
2023-01-31 18:40:56 +0300

    Added implementations to Observer Pattern

--- task2-observer-pattern/src/main/java/com/mjc/stage2/entity/Rectangle.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/entity/Rectangle.java
@@ -1,9 +1,16 @@
 package com.mjc.stage2.entity;
 
-public class Rectangle {
+import com.mjc.stage2.Observable;
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.event.RectangleEvent;
+
+import java.util.List;
+
+public class Rectangle implements Observable {
     private int id;
     private double sideA;
     private double sideB;
+    private List<Observer> observers;
 
     // Write your code here!
     public Rectangle(int id, double sideA, double sideB) {
@@ -26,6 +33,7 @@ public class Rectangle {
 
     public void setSideA(double sideA) {
         this.sideA = sideA;
+        notifyObserver();
         // Write your code here!
     }
 
@@ -35,7 +43,26 @@ public class Rectangle {
 
     public void setSideB(double sideB) {
         this.sideB = sideB;
+        notifyObserver();
         // Write your code here!
+    }
+
+    @Override
+    public void addObserver(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObserver() {
+        RectangleEvent rectangleEvent = new RectangleEvent(this);
+        for (Observer observer : observers) {
+            observer.handleEvent(rectangleEvent);
+        }
     }
 // Write your code here!
 }
--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,28 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+public class RectangleObserver implements Observer {
+
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        RectangleWarehouse warehouse = RectangleWarehouse.getInstance();
+        Rectangle source = event.getSource();
+
+        int id = source.getId();
+        double rectangleSquare = source.getSideA() * source.getSideB();
+        double rectanglePerimeter = source.getSideA() * 2 + source.getSideB() * 2;
+        RectangleValues rectangleValues = warehouse.get(id);
+        if (rectangleValues != null) {
+            rectangleValues.setSquare(rectangleSquare);
+            rectangleValues.setPerimeter(rectanglePerimeter);
+        } else {
+            warehouse.put(id, new RectangleValues(rectangleSquare, rectanglePerimeter));
+        }
+    }
     // Write your code here!
 }
